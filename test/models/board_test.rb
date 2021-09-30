73c4128927f455c934443ab09a26c21d1779bcd0
Wenyi He <whe1@uc.cl>
2021-09-29 23:40:01 -0300

    ✅ Add tests for some board code

--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -11,10 +11,10 @@ class BoardTest < Test::Unit::TestCase
     @expected = [
       [0, 0, 0, 0, 0, 0],
       [1, 2, 1, 1, 0, 0],
-      [1, 2, 1, 2, 1, 1],
-      [2, 3, 1, 2, 1, 1],
-      [1, 1, 1, 3, 3, 2],
-      [1, 1, 1, 2, 2, 1]
+      [1, 3, 1, 2, 1, 1],
+      [3, 3, 2, 2, 0, 1],
+      [2, 3, 3, 3, 4, 3],
+      [2, 1, 2, 1, 2, 1]
     ]
   end
 
@@ -26,7 +26,6 @@ class BoardTest < Test::Unit::TestCase
   def test_board_neighbors_data
     @board.matrix.each_with_index do |row, i|
       row.each_with_index do |cell, j|
-        puts "#{cell.neighbor_bombs} #{@expected[i][j]}"
         assert_equal(cell.neighbor_bombs, @expected[i][j], 'Incorrect amount of bombs around cell')
       end
     end
@@ -36,6 +35,26 @@ class BoardTest < Test::Unit::TestCase
     @board.matrix.each_with_index do |row, i|
       row.each_with_index do |cell, j|
         assert_equal(cell, @board.get_cell(i, j), 'Incorrect cell gotten from board')
+      end
+    end
+  end
+
+  def test_discover_cell
+    @board.discover_cell(1, 1)
+    assert_equal(true, @board.matrix[1][1].is_open)
+    @board.matrix.each_with_index do |row, i|
+      row.each_with_index do |cell, j|
+        puts "#{cell.is_open} #{i} #{i} "
+
+        assert_equal(false, cell.is_open, 'Cell is unvealed incorrectly') unless j == 1 && i == 1
+      end
+    end
+  end
+
+  def test_check_is_bomb
+    @board.matrix.each_with_index do |row, i|
+      row.each_with_index do |cell, j|
+        assert_equal(cell.is_bomb, @board.check_is_bomb(i, j), 'Bomb reported incorrectly')
       end
     end
   end
