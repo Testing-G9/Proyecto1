450f38cecefc216042fa5a6b541faf8665271780
Wenyi He <whe1@uc.cl>
2021-10-02 18:41:30 -0300

    ✅ Add tests for Board Model
    Co-authored-by: Leonardo Olivares <leolivares@uc.cl>
    Co-authored-by: Janoda <Janoda@users.noreply.github.com>
    Co-authored-by: Sebastian Ricke <sricke@uc.cl>

--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -4,18 +4,18 @@ require_relative '../test_helper'
 require_relative '../../lib/models/board'
 require 'test/unit'
 
+# Class for testing the board model
 class BoardTest < Test::Unit::TestCase
   def setup
     srand(31_415)
-    @board = Board.new(6)
-    @expected = [
-      [0, 0, 0, 0, 0, 0],
-      [1, 2, 1, 1, 0, 0],
-      [1, 3, 1, 2, 1, 1],
-      [3, 3, 2, 2, 0, 1],
-      [2, 3, 3, 3, 4, 3],
-      [2, 1, 2, 1, 2, 1]
-    ]
+    @size = 6
+    @board = Board.new(@size)
+    @expected = [[0, 0, 0, 0, 0, 0],
+                 [1, 2, 1, 1, 0, 0],
+                 [1, 3, 1, 2, 1, 1],
+                 [3, 3, 2, 2, 0, 1],
+                 [2, 3, 3, 3, 4, 3],
+                 [2, 1, 2, 1, 2, 1]]
   end
 
   def test_board_size
@@ -44,9 +44,20 @@ class BoardTest < Test::Unit::TestCase
     assert_equal(true, @board.matrix[1][1].is_open)
     @board.matrix.each_with_index do |row, i|
       row.each_with_index do |cell, j|
-        puts "#{cell.is_open} #{i} #{i} "
+        assert_equal(false, cell.is_open, 'Cell is unveiled incorrectly') unless j == 1 && i == 1
+      end
+    end
+  end
 
-        assert_equal(false, cell.is_open, 'Cell is unvealed incorrectly') unless j == 1 && i == 1
+  def test_discover_cell_recursive
+    @board.discover_board(0, 0)
+    @board.matrix.each_with_index do |row, i|
+      row.each_with_index do |cell, j|
+        if [0, 1].include?(i) || [[2, 4], [2, 5]].include?([i, j])
+          assert_equal(true, cell.is_open, 'Cell is unveiled incorrectly')
+        else
+          assert_equal(false, cell.is_open, 'Cell is unveiled incorrectly')
+        end
       end
     end
   end
@@ -57,5 +68,61 @@ class BoardTest < Test::Unit::TestCase
         assert_equal(cell.is_bomb, @board.check_is_bomb(i, j), 'Bomb reported incorrectly')
       end
     end
+  end
+
+  def test_mark_cell_discover
+    @board.mark_cell(0, 1)
+    assert_equal(true, @board.get_cell(0, 1).is_open, 'Cell is unveiled incorrectly')
+    assert_equal(true, @board.get_cell(0, 0).is_open, 'Cell is unveiled incorrectly')
+    assert_equal(true, @board.get_cell(0, 2).is_open, 'Cell is unveiled incorrectly')
+    assert_equal(false, @board.get_cell(3, 2).is_open, 'Cell is unveiled incorrectly')
+  end
+
+  def test_border_condition
+    size_arr = *(-1...@size)
+    size_arr.repeated_permutation(4).each do |i_pos, j_pos, row, col|
+      condition = (i_pos + row).between?(0, @size - 1) && (j_pos + col).between?(0, @size - 1)
+      assert_equal(!condition, @board.border_condition(i_pos, j_pos, row, col))
+    end
+  end
+
+  def test_orthogonal
+    neighbor_arr = *(-1..1)
+    neighbor_arr.repeated_permutation(2).each do |i_pos, j_pos|
+      condition = (i_pos - j_pos).abs == 1
+      assert_equal(!condition, @board.orthogonal?(i_pos, j_pos).zero?)
+    end
+  end
+
+  # rubocop:disable Metrics/AbcSize
+  def test_discovered_condition
+    @board.discover_cell(1, 1)
+    neighbor_arr = *(-1..1)
+    matrix_arr = *(0...@size)
+    matrix_arr.repeated_permutation(2).each do |i, j|
+      neighbor_arr.repeated_permutation(2).each do |i_pos, j_pos|
+        next unless (i + i_pos).between?(0, @size - 1) && (j + j_pos).between?(0, @size - 1)
+
+        assert_equal(i == 1 && j == 1, @board.discovered_condition(i_pos, j_pos, i - i_pos, j - j_pos),
+                     'Neighbor Cell is unveiled incorrectly')
+      end
+    end
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  def test_unveil_bombs
+    @board.unveil_bombs
+    @board.matrix.each do |row|
+      row.each do |cell|
+        assert_equal(cell.is_open, true) if cell.is_bomb
+      end
+    end
+  end
+
+  def test_unveil_position_check_border
+    assert_equal(true, @board.unveil_position_check(0, 1, -1, 0), 'Border condition incorrect')
+    assert_equal(true, @board.unveil_position_check(1, 0, 0, -1), 'Border condition incorrect')
+    assert_equal(true, @board.unveil_position_check(5, 0, 1, 0), 'Border condition incorrect')
+    assert_equal(true, @board.unveil_position_check(0, 5, 0, 1), 'Border condition incorrect')
   end
 end
