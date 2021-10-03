74c1237da00349c38fe772d5463db82af5d7d6d0
Joaquin <jiaraya4@uc.cl>
2021-10-03 18:31:23 -0300

    :white_check_mark: added test__check_win

--- test/controllers/game_test.rb
+++ test/controllers/game_test.rb
@@ -32,12 +32,26 @@ class GameTest < Test::Unit::TestCase
   end
 
   def test_mark_cell
-    @game.model.mark_cell(3,3)
+    silenced do
+      @game.mark_cell(3, 3)
+    end
     assert_equal(true, @game.model.matrix[3][3].is_open)
     @game.model.matrix.each_with_index do |row, i|
       row.each_with_index do |cell, j|
         assert_equal(false, cell.is_open, 'Cell is unveiled incorrectly') unless j == 3 && i == 3
       end
     end
+  end
+
+  def test_check_win
+    @game.model.expects(:unveil_bombs).returns.at_least_once
+    @game.model.matrix.each_with_index do |row, i|
+      row.each_with_index do |cell, j|
+        silenced do
+          @game.model.mark_cell(i, j) unless cell.is_open || cell.is_bomb
+        end
+      end
+    end
+    assert_equal(true, @game.check_win)
   end
 end
